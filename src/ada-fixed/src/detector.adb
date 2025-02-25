c09c1eac2c73b8a68003ae3d442ff9d0cfe5a410
joseaverde <joseaverde@protonmail.com>
2025-02-25 12:42:45 +0100

    Proved sum

--- src/ada-fixed/src/detector.adb
+++ src/ada-fixed/src/detector.adb
@@ -79,8 +79,8 @@ package body Detector with SPARK_Mode => On is
       Min : Sample_Type := Item (Item'First);
    begin
       for Index in Item'First + 1 .. Item'Last loop
-         pragma Loop_invariant (Max = Acc_Maximum (Item) (Index - 1));
-         pragma Loop_invariant (Min = Acc_Minimum (Item) (Index - 1));
+         pragma Loop_Invariant (Max = Acc_Maximum (Item) (Index - 1));
+         pragma Loop_Invariant (Min = Acc_Minimum (Item) (Index - 1));
          Max := Sample_Type'Max (Item (Index), Max);
          Min := Sample_Type'Min (Item (Index), Min);
       end loop;
@@ -88,5 +88,76 @@ package body Detector with SPARK_Mode => On is
       pragma Assert (Min = Minimum (Item));
       return Feature_Type (Max) - Feature_Type (Min);
    end Max_Distance;
+
+   -->> Energy <<--
+
+   function Acc_Sum (
+      Item : in Sample_Epoch)
+      return Sample_Base_Epoch is
+      Result : Sample_Base_Epoch := [others => 0.0];
+
+      pragma Warnings (Off);
+      function Lemma_Sample_Increment (
+         Left  : in Sample_Base_Type;
+         Right : in Sample_Base_Type;
+         Index : in Count_Type)
+         return Boolean with
+         Pre      => Index in 1 .. Epoch_Size - 1
+            and then Left in Sample_Base_Type (Index) * Sample_Type'First
+                          .. Sample_Base_Type (Index) * Sample_Type'Last
+            and then Right in Sample_Type,
+         Post     => Lemma_Sample_Increment'Result = True
+            and then Left + Right
+                        in Sample_Base_Type (Index + 1) * Sample_Type'First
+                        .. Sample_Base_Type (Index + 1) * Sample_Type'Last;
+      pragma Warnings (On);
+
+      function Lemma_Sample_Increment (
+         Left  : in Sample_Base_Type;
+         Right : in Sample_Base_Type;
+         Index : in Count_Type)
+         return Boolean is (True);
+
+   begin
+      Result (Item'First) := Item (Item'First);
+      pragma Assert (Result (Item'First) in Sample_Type);
+      for Index in Item'First + 1 .. Item'Last loop
+         pragma Loop_Invariant (Result (Item'First) = Item (Item'First));
+         pragma Loop_Invariant (
+            (for all I in Item'First + 1 .. Index - 1
+               =>       Result (I - 1)
+                           in Sample_Base_Type (I - 1) * Sample_Type'First
+                           .. Sample_Base_Type (I - 1) * Sample_Type'Last
+               and then Lemma_Sample_Increment (Result (I - 1), Item (I),
+                                                I - 1)
+               and then Result (I) = Result (I - 1) + Item (I)
+               and then Result (I)
+                           in Sample_Base_Type (I) * Sample_Type'First
+                           .. Sample_Base_Type (I) * Sample_Type'Last));
+         Result (Index) := Result (Index - 1) + Item (Index);
+      end loop;
+      return Result;
+   end Acc_Sum;
+
+-- function Mean (
+--    Item : in Sample_Epoch)
+--    return Sample_Type is
+-- begin
+--    return 0.0;
+-- end Mean;
+
+-- function Energy (
+--    Item : in Sample_Epoch)
+--    return Feature_Type is
+--    μ      : constant Sample_Type := Mean (Item);
+--    Value  : Feature_Type;
+--    Result : Feature_Type := 0.0;
+-- begin
+--    for I in Item'Range loop
+--       Value := Feature_Type (Signal (I)) - Feature_Type (μ);
+--       Result := Result + Value;
+--    end loop;
+--    return Result / Feature_Type (Signal'Length);
+-- end Energy;
 
 end Detector;
--- src/ada-fixed/src/detector.ads
+++ src/ada-fixed/src/detector.ads
@@ -5,15 +5,54 @@ package Detector with SPARK_Mode => On is
    Stride_Size : constant := 256;
    Epoch_Size  : constant := 1280;
 
-   type Count_Type is range 0 .. 1_000_000;
+   type Count_Type is range 0 .. 1_000_000 with Size => Bits;
    subtype Index_Type is Count_Type range 1 .. Count_Type'Last;
-   type Sample_Type is delta 2.0 ** (-4) range -10_000.0 .. 10_000.0;
+
+   -- Samples --
+
+   Sample_Mantissa : constant := 4;
+   Sample_Delta    : constant := 2.0 ** (-Sample_Mantissa);
+
+   type Sample_Base_Type is
+      delta Sample_Delta
+      range -2.0 ** (Bits - Sample_Mantissa - 1)
+         .. 2.0 ** (Bits - Sample_Mantissa - 1) - Sample_Delta with
+      Size => Bits;
+
+   subtype Sample_Type is Sample_Base_Type range -10_000.0 .. 10_000.0;
+
+   type Sample_Base_Array is array (Index_Type range <>) of Sample_Base_Type;
+   subtype Sample_Base_Epoch is Sample_Base_Array (1 .. Epoch_Size);
    type Sample_Array is array (Index_Type range <>) of Sample_Type;
    subtype Sample_Epoch is Sample_Array (1 .. Epoch_Size);
 
-   type Feature_Type is delta 2.0 ** (-10) range -20_000.0 .. 20_000.0;
+   -- Features --
+
+   type Feature_Type is delta 2.0 ** (-10) range -20_000.0 .. 20_000.0 with
+      Size => Bits;
 
    -->> Max distance <<--
+   -- The Max_Distance is a function that returns the difference between the
+   -- maximum value on an Epoch and the minimum value of an Epoch. Then if
+   -- the domain is:
+   --
+   --    Domain is range First .. Last
+   --
+   -- We now that:
+   --
+   --    Maximum (Epoch) >= Minimum (Epoch)
+   --
+   -- Then if:
+   --
+   --    Maximum (Epoch) = Minimum (Epoch) => Result := 0.0
+   --
+   -- If Maximum (Epoch) = Last, and Minimum (Epoch) = First then:
+   --
+   --    Result := Last - First
+   --
+   -- Which maximises the function. Then we now that the codomain will be:
+   --
+   --    Codomain is range 0 .. Last - First
 
    function Acc_Maximum (
       Item : in Sample_Epoch)
@@ -65,5 +104,30 @@ package Detector with SPARK_Mode => On is
          and then Max_Distance'Result in 0.0
                                       .. Feature_Type (Sample_Type'Last)
                                          - Feature_Type (Sample_Type'First);
+
+   -->> Mean <<--
+
+   function Acc_Sum (
+      Item : in Sample_Epoch)
+      return Sample_Base_Epoch with
+      Ghost    => True,
+      Global   => null,
+      Post     => Acc_Sum'Result (Item'First) = Item (Item'First)
+         and then (for all I in Item'Range =>
+                     Acc_Sum'Result (I)
+                        in Sample_Base_Type (I) * Sample_Type'First
+                        .. Sample_Base_Type (I) * Sample_Type'Last)
+         and then (for all I in Item'First + 1 .. Item'Last =>
+                     Acc_Sum'Result (I) = Acc_Sum'Result (I - 1) + Item (I));
+
+-- function Mean (
+--    Item : in Sample_Epoch)
+--    return Sample_Type;
+
+-- function Energy (
+--    Item : in Sample_Epoch)
+--    return Feature_Type with
+--    Global => null;
+--    -- Post => ;
 
 end Detector;
